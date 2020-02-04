5c4e864821e228e289b00953af89f303b3c27ffd
tsp <tamalesp@gmail.com>
2020-02-04 15:03:03 +0300

    second reactjs commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, My Name is Simon Peter Tamale.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "Hello Simon Peter Tamale"
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button
+            onClick={() => this.setState({ string: "Hello Zhavia Namuli" })}
+          >
+            Change text
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
