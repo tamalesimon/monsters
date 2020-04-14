8a7f299990b509c7698667af15571a1e1b24ca27
tsp <tamalesp@gmail.com>
2020-04-14 21:40:52 +0300

    final touches react lesson

--- src/App.js
+++ src/App.js
@@ -39,6 +39,8 @@ class App extends Component {
     return (
       <div className="App">
 
+      <h1>Monster Rolodex</h1>
+
         <SearchBox
         placeholder='Search Monsters'
         handleChange={this.handleChange}  
