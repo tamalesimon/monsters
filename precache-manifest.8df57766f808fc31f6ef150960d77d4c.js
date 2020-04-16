5221b344387dce5afb95a1a9028ce4fac65671ca
tsp <tamalesp@gmail.com>
2020-04-17 02:44:20 +0300

    Updates

---
+++ precache-manifest.8df57766f808fc31f6ef150960d77d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91f625274cd6f9d60bd854d6f1b59b0",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5f335114c4118a06e53b",
+    "url": "/monsters/static/css/main.7bf80eef.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f335114c4118a06e53b",
+    "url": "/monsters/static/js/main.c72b0d61.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
